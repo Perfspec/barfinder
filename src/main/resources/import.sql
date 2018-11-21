217121bf7e749d63c8e595b62ed88e6a8caf7bd0
Perfspec <lewisrwebb@gmail.com>
2018-11-21 16:10:07 +0000

    h2 console calls the information from import.sql effectively

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO Brewery(logoUrl, name) VALUES("urlsomething1","Brewery1");
-INSERT INTO Drink(abv, idBrewery, logoUrl, name) VALUES(3.8, 1, "urlsomething2", "WhiskySour");
-INSERT INTO Bar(name,address,postcode,easting,northing,latitude,longitude,local_authority) VALUES("Anglesey Arms","Halnaker, Boxgrove, West Sussex","PO18 0NQ",490846,108134,50.865585,-0.710445,"Chichester");
+INSERT INTO Brewery(name,logoUrl) VALUES('Harveys','https://pbs.twimg.com/profile_images/859330003103862784/zS4k0gJG.jpg');
+INSERT INTO Drink(name,logoUrl,idBrewery,abv) VALUES('Sussex Best Bitter', 'https://d1dh7zebtp17c7.cloudfront.net/20151026093216/Polypin-Best-Bitter-2016.jpg',1,4.0);
+INSERT INTO Bar(name,address,postcode,easting,northing,latitude,longitude,local_authority) VALUES('Anglesey Arms','Halnaker, Boxgrove, West Sussex','PO18 0NQ',490846,108134,50.865585,-0.710445,'Chichester');
 INSERT INTO Offer(idBar,idBrewery,idDrink,price) VALUES(1,1,1,2.5);
