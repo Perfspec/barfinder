3156e7aec59e00ea3a510b69d42f13edb7db6678
Perfspec <lewisrwebb@gmail.com>
2018-11-25 20:41:17 +0000

    bst instead of bst2

--- front-end/src/BrewPage.js
+++ front-end/src/BrewPage.js
@@ -1,32 +1,53 @@
 import React, {Component} from 'react';
-import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table-next';
-import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
-
-const getAll = [{
-  "idBrewery":1,
-  "name":"Harveys",
-  "logoUrl":"https://pbs.twimg.com/profile_images/859330003103862784/zS4k0gJG.jpg"
-},{
-  "idBrewery":2,
-  "name":"BrewDog",
-  "logoUrl":"https://pbs.twimg.com/profile_images/586475073034330112/kNHqVAx6_400x400.jpg"
-}];
-
-const cellEditProp = {
-  mode: 'click'
-};
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import 'react-bootstrap-table/dist/react-bootstrap-table.min.css';
 
 class BrewPage extends Component {
-
-    render() {
-      return (
-        <BootstrapTable data={ getAll } cellEdit={ cellEditProp }>
-          <TableHeaderColumn dataField='idBrewery' isKey>id</TableHeaderColumn>
-          <TableHeaderColumn dataField='name'>name</TableHeaderColumn>
-          <TableHeaderColumn dataField='logoUrl'>logoUrl</TableHeaderColumn>
-        </BootstrapTable>
-            );
+  constructor(props){
+    super(props);
+    this.state = {
+      tableArray: this.props.data
     }
+  }
+
+
+  render() {
+    return (
+      <BootstrapTable
+        id="tableList"
+        data={this.state.tableArray}
+        className="table table-striped"
+        search
+        scrollable >
+
+        <TableHeaderColumn
+          width= '100'
+          dataField='idBrewery'
+          isKey
+          dataSort
+          dataAlign='center'>
+          Id
+        </TableHeaderColumn>
+
+        <TableHeaderColumn
+          width= '100'
+          dataField='name'
+          dataSort
+          dataAlign='center'>
+          Name
+        </TableHeaderColumn>
+
+        <TableHeaderColumn
+          width= '200'
+          dataField='logoUrl'
+          dataSort
+          dataAlign='center'>
+          Logo
+        </TableHeaderColumn>
+        
+      </BootstrapTable>
+    );
+  }
 }
 
 export {BrewPage};
--- front-end/src/PageSelector.js
+++ front-end/src/PageSelector.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import {BrewPage} from './BrewPage.js';
 import {BrewAddUpdDelButtons} from './BrewAddUpdDelButtons.js'
+import Axios from 'axios';
+import {BREWERY, ALL} from './const.js';
 
 class PageSelector extends Component {
+  brewAll = () => {
+    Axios.get(BREWERY+ALL).then(response => {return response;});
+  }
+
   render() {
     if(this.props.selector==='Brewery'){
       return (
         <div id='PageSelected==Brewery'>
         <BrewAddUpdDelButtons />
-        <BrewPage />
+        <BrewPage data={this.brewAll}/>
         </div>
       );
     } else {
